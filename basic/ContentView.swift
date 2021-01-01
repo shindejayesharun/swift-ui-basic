1c06cab8671d9b2214e9a295bf6816f2e85ef02b
Shinde Jayesh Arun <shindejayesharun@gmail.com>
2021-01-01 22:31:25 +0530

    Initial Commit

---
+++ basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basic
+//
+//  Created by Profcyma on 01/01/21.
+//  Copyright © 2021 imac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
